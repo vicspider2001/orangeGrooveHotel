b47f6a72060dc607abbafe9de7bdc07359c7421d
Victor Anogwih <anogwihvictor@gmail.com>
2024-05-04 23:07:36 -0400

    HtoelWebApp

---
+++ precache-manifest.1ab445074672102427536fa2e0cb7249.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fe954511648e37008ef40b362a1f577",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94632fbf7bf5e5d3b527",
+    "url": "/static/css/2.7ae7b8b0.chunk.css"
+  },
+  {
+    "revision": "96e7589dcc96835e87d5",
+    "url": "/static/css/main.94e1c82a.chunk.css"
+  },
+  {
+    "revision": "94632fbf7bf5e5d3b527",
+    "url": "/static/js/2.0dca28da.chunk.js"
+  },
+  {
+    "revision": "96e7589dcc96835e87d5",
+    "url": "/static/js/main.be33c5c6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
